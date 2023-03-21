d6aa72fddb9853b83340e27ccfd754a7999d440e
Polina <125902170+Polina24zinoveva@users.noreply.github.com>
2023-03-21 17:14:25 +0400

    Add files via upload

---
+++ Основы_программирования/second_semester/LR5/IVectorable.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OP_Laboratornaya5
+{
+    interface IVectorable
+    {
+        public int this[int index]
+        {
+            get;
+            set;
+        }
+
+        public int Length
+        {
+            get;
+        }
+
+        public double GetNorm();
+        public int CompareTo(IVectorable vector);
+        public object Clone();
+    }
+}
---
+++ Основы_программирования/second_semester/LR5/Program.cs
@@ -0,0 +1,404 @@
+﻿using System;
+
+namespace OP_Laboratornaya5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Task();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error while executing program:\n" + e.Message);
+            }
+        }
+
+        static void Task()
+        {
+            Console.WriteLine("Лабораторная работа №5\nВыполнила: Зиновьева Полина, студентка группы 6103-020302D");
+            while (true)
+            {
+                Console.WriteLine("Выберете с чем работать:\n1 - ArrayVector\n" +
+                                  "2 - LinkedListVector\n3 - Задания лабораторной №5\n0 - выход");
+                string menu = Console.ReadLine();
+                Console.Clear();
+                switch (menu)
+                {
+                    case "0":
+                        return;
+                    case "1":
+                        Task1();
+                        break;
+                    case "2":
+                        Task2();
+                        break;
+                    case "3":
+                        try
+                        {
+                            Task3();
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.Message);
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("Такого пункта меню нет. Повторите ввод");
+                        break;
+                }
+            }
+        }
+
+        static void Task1() //ArrayVector
+        {
+            Console.WriteLine("ArrayVector:");
+            ArrayVector vector = ReadVectorA();
+            while (true)
+            {
+                Console.WriteLine("Выберете задание:\n1 - Вывести элемент по индексу\n2 - Ввести элемент по индексу\n" +
+                    "3 - Узнать модуль вектора\n4 - Узнать число координат вектора и его значения\n0 - выход");
+                string menu = Console.ReadLine();
+                Console.Clear();
+                switch (menu)
+                {
+                    case "0":
+                        return;
+                    case "1":
+                        try
+                        {
+                            int index = VvodIndexa();
+                            Console.WriteLine("Элемент под" + index + " индексом: " + vector[index]);
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.Message);
+                        }
+                        break;
+                    case "2":
+                        try
+                        {
+                            Console.WriteLine("Исходный вектор" + vector);
+                            int index = VvodIndexa();
+                            int chislo = VvodChisla();
+                            vector[index] = chislo;
+                            Console.WriteLine("Полученный вектор" + vector);
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.Message);
+                        }
+                        break;
+                    case "3":
+                        Console.WriteLine("Модуль вектора: " + vector.GetNorm());
+                        break;
+                    case "4":
+                        Console.WriteLine(vector);
+                        break;
+                    default:
+                        Console.WriteLine("Такого пункта меню нет. Повторите ввод");
+                        break;
+                }
+            }
+
+        }
+
+        static void Task2() //LinkedListVector
+        {
+            Console.WriteLine("LinkedListVector:");
+            LinkListVector vector = ReadVectorL();
+
+            while (true)
+            {
+                Console.WriteLine("Выберете задание:\n1 - Узнать модуль вектора\n2 - Узнать число координат вектора\n" +
+                                  "3 - Добавить элемент в начало\n4 - Добавить элемент в конец\n5 - Удалить элемент из начала\n" +
+                                  "6 - Удалить элемент из конца\n7 - Добавить элемент в заданную позицию\n" +
+                                  "8 - Удалить элемент из заданной позиции\n0 - выход");
+                string menu = Console.ReadLine();
+                Console.Clear();
+                switch (menu)
+                {
+                    case "0":
+                        return;
+                    case "1":
+                        Console.WriteLine("Модуль вектора: " + vector.GetNorm());
+                        break;
+                    case "2":
+                        Console.WriteLine("Число координат вектора: " + vector.Length);
+                        break;
+                    case "3":
+                        try
+                        {
+                            vector.AddToHead(VvodChisla());
+                            Console.WriteLine("Вектор: " + vector);
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.Message);
+                        }
+                        break;
+                    case "4":
+                        try
+                        {
+                            vector.AddToTail(VvodChisla());
+                            Console.WriteLine("Вектор: " + vector);
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.Message);
+                        }
+                        break;
+                    case "5":
+                        vector.RemoveFromHead();
+                        Console.WriteLine("Вектор: " + vector);
+                        break;
+                    case "6":
+                        vector.RemoveFromTail();
+                        Console.WriteLine("Вектор: " + vector);
+                        break;
+                    case "7":
+                        try
+                        {
+                            vector.AddAtPosition(VvodIndexa(), VvodChisla());
+                            Console.WriteLine("Вектор: " + vector);
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.Message);
+                        }
+                        break;
+                    case "8":
+                        try
+                        {
+                            vector.RemoveAtPosition(VvodIndexa());
+                            Console.WriteLine("Вектор: " + vector);
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.Message);
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("Такого пункта меню нет. Повторите ввод");
+                        break;
+                }
+            }
+        }
+
+        static void Task3()
+        {
+            Console.Write("Введите размер массива: ");
+            int razmer = Convert.ToInt32(Console.ReadLine()); 
+            IVectorable[] mass = new IVectorable[razmer];
+            for (int j = 0; j < razmer; j++)
+            {
+                Console.WriteLine("Какой тип вектора? 1 - ArrayVector; 2 - LinkListVector");
+                Console.Write("Выбранное задание: ");
+                string menu = Console.ReadLine();
+                Console.Clear();
+                switch (menu)
+                {
+                    case "0":
+                        return;
+                    case "1":
+                        { 
+                            mass[j] = ReadVectorA();
+                        }
+                        break;
+                    case "2":
+                        {
+                            mass[j] = ReadVectorL();
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            Console.WriteLine("Вектор с минимальным числом координат: ");
+            IVectorable min = mass[0];
+            for (int i = 0; i < mass.Length-1; i ++)
+            {
+                if (min.CompareTo(mass[i+1]) == 1)
+                {
+                    min = mass[i + 1];
+                }
+            }
+            Console.WriteLine(min);
+
+            Console.WriteLine("Вектор с максимальным числом координат: ");
+            IVectorable max = mass[0];
+            for (int i = 0; i < mass.Length-1; i++)
+            {
+                if (max.CompareTo(mass[i + 1]) == -1)
+                {
+                    max = mass[i + 1];
+                }
+            }
+            Console.WriteLine(max);
+
+            Console.WriteLine("Массив до сортировки по возрастанию модулей: ");
+            Print(mass);
+            Array.Sort(mass, new Comparer());
+            Console.WriteLine("Массив после сортировки по возрастанию модулей: ");
+            for (int i = 0; i < mass.Length; i++)
+            {
+                Console.WriteLine(i + " элемент: " + mass[i] + ", модуль: " + mass[i].GetNorm());
+            }
+
+            int k = 0;
+            try
+            {
+                Console.WriteLine("Какой вектор хотите клонировать?");
+                k = Convert.ToInt32(Console.ReadLine());
+                if ((k < 0) || (k >= mass.Length))
+                {
+                    Console.WriteLine("Неверный вектор. Произойдет клонирование 0 вектора");
+                    k = 0; 
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            IVectorable ishodnuy = mass[k];
+            Console.Write("Исходный вектор: " + ishodnuy + "\n");
+            IVectorable klon = (IVectorable)mass[k].Clone();
+            Console.WriteLine("Клонированный вектор: " + klon);
+
+            Console.WriteLine("Проверка клона:");
+            try
+            {
+                Console.WriteLine("Какой элемент заменить?");
+                int index = VvodIndexa();
+                if (index >= klon.Length)
+                {
+                    throw new Exception("Неверный номер элемента");
+                }
+                int znachenie = VvodChisla();
+                klon[index] = znachenie;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.Write("Исходный вектор: " + ishodnuy + "\n");
+            Console.WriteLine("Клонированный вектор: " + klon);
+
+            if (klon.Equals(ishodnuy))
+            {
+                Console.WriteLine("Векторы равны!");
+            }
+            else
+            {
+                Console.WriteLine("Векторы не равны!");
+            }  
+        }
+
+        private static ArrayVector ReadVectorA()
+        {
+            Console.Write("Вы будете вводить длину массива? \n1 - да, 0 - нет: ");
+            string x = Console.ReadLine();
+            ArrayVector vector;
+            if ((x == "1") || (x == "0"))
+            {
+                if (x == "1")
+                {
+                    Console.Write("Введите длину вектора: ");
+                    int razmer = Convert.ToInt32(Console.ReadLine());
+                    if (razmer < 1)
+                    {
+                        Console.WriteLine("Неправильная длина вектора!");
+                        throw new Exception();
+                    }
+                    else
+                    {
+                        vector = new ArrayVector(razmer);
+                    }
+                }
+                else
+                {
+                    vector = new ArrayVector();
+                }
+                for (int i = 0; i < vector.Length; i++)
+                {
+                    Console.Write("Введите " + i + " элемент: ");
+                    int chislo = Convert.ToInt32(Console.ReadLine());
+                    vector[i] = chislo;
+                }
+
+                return vector;
+            }
+            else
+            {
+                throw new Exception("Неверное значение!");
+            }
+        }
+        private static LinkListVector ReadVectorL()
+        {
+            Console.Write("Вы будете вводить длину массива? \n1 - да, 0 - нет: ");
+            string x = Console.ReadLine();
+            LinkListVector vector;
+            if ((x == "1") || (x == "0"))
+            {
+                if (x == "1")
+                {
+                    Console.Write("Введите длину вектора: ");
+                    int razmer = Convert.ToInt32(Console.ReadLine());
+                    if (razmer < 1)
+                    {
+                        throw new Exception("Неправильная длина вектора!");
+                    }
+                    else
+                    {
+                        vector = new LinkListVector(razmer);
+                    }
+                }
+                else
+                {
+                    vector = new LinkListVector();
+                }
+
+                for (int i = 0; i < vector.Length; i++)
+                {
+                    Console.Write("Введите " + i + " элемент: ");
+                    int chislo = Convert.ToInt32(Console.ReadLine());
+                    vector[i] = chislo;
+                }
+                return vector;
+            }
+            else
+            {
+                throw new Exception("Неверное значение!");
+            }
+
+        }
+        private static int VvodChisla()
+        {
+            Console.Write("Введите целое число: ");
+            int value = Convert.ToInt32(Console.ReadLine());
+            return value;
+        }
+        private static int VvodIndexa()
+        {
+            Console.Write("Введите индекс: ");
+            int index = Convert.ToInt32(Console.ReadLine());
+            if (index < 0)
+            {
+                throw new Exception("Error while executing program:\n" + "Введен отрицательный индекс! " + index);
+            }
+            return index;
+        }
+        public static void Print(IVectorable[] mass)
+        {
+            for (int i = 0; i < mass.Length; i++)
+            {
+                Console.WriteLine(i + " элемент: " + mass[i]);
+            }
+        }
+    }
+    
+}
